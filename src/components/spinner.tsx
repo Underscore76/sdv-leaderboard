f2431fc984dc082ec650cb2cb9b3286be869d3e4
Underscore76 <theunderscore76@gmail.com>
2023-09-02 16:54:55 -0500

    fix spinner

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,3 +1,4 @@
+import chicken from "../assets/spinner.png";
 export default function ChickenSpinner() {
   return (
     <div
@@ -5,7 +6,7 @@ export default function ChickenSpinner() {
       className="absolute left-1/2 top-2/4 -translate-x-1/2 -translate-y-1/2"
     >
       <img
-        src="/spinner.png"
+        src={chicken}
         className="animate-reverse-spin mr-2 h-24 w-24 fill-blue-600 text-gray-200 dark:text-gray-600"
       />
     </div>
